1ca8357f3e7f1db441367aa900823017d02d3903
Heather Rodriguez <heather.rodriguez@docusign.com>
2020-07-21 22:11:28 -0700

    Adding directories and configuring gulp file

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+//Compile Sass
+gulp.task('sass', function(){
+    return gulp.src('src/scss/**/*.scss')
+      .pipe(sass()) 
+      .pipe(gulp.dest('src/css'))
+  });
+
+//Watch for changes
+gulp.task('watch', function(){
+    gulp.watch('src/scss/**/*.scss', gulp.series(['sass'])); 
+  })
