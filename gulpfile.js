cdb5319741e4ea3f5eea05ad0ef6433e4aac284b
Heather Rodriguez <heather.rodriguez@docusign.com>
2020-07-22 20:27:47 -0700

    Setting up folder structure and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var sass = require('gulp-sass');
 gulp.task('sass', function(){
     return gulp.src('src/scss/**/*.scss')
       .pipe(sass()) 
-      .pipe(gulp.dest('src/css'))
+      .pipe(gulp.dest('dist/css'))
   });
 
 //Watch for changes
